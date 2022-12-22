f95b20b7200dc508d2c62ce70d9251faafa63329
alena-suhih <prhalenka23@gmail.com>
2022-12-22 19:01:21 +0200

    task 1 and task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,38 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let discreminant = Math.pow(b, 2) - 4 * a * c;
+  if (discreminant < 0) {
+    arr = []
+  } else if (discreminant === 0) {
+    arr = [-b / (2 * a)]
+  } else if (discreminant > 0) {
+    arr = [(-b + Math.sqrt(discreminant)) / (2 * a), (-b - Math.sqrt(discreminant)) / (2 * a)]
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  percent = Number(percent)
+  contribution = Number(contribution)
+  amount = Number(amount)
+  countMonths = Number(countMonths)
+  if (isNaN(percent) === true) {
+    return false
+  }
+  if (isNaN(contribution) === true) {
+    return false
+  }
+  if (isNaN(amount) === true) {
+    return false
+  }
+  if (isNaN(countMonths) === true) {
+    return false
+  }
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  let percentMonth = (percent / 100) / 12
+  let loanBody = amount - contribution
+  let monthlyPayment = loanBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)))
+  let totalSum = Number((monthlyPayment * countMonths).toFixed(2))
+  return totalSum
 }
