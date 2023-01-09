9169813a7e70679f57340cff13d84e3f0f2c8628
alena-suhih <prhalenka23@gmail.com>
2023-01-09 19:07:35 +0200

    improve 1 and 2 tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,15 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false
-    } else if (arr1.every ((element, index) => element === arr2[index])) {
+    if (arr1.length === arr2.length && arr1.every ((element, index) => element === arr2[index])){
         return true
-    } else return false
+    } 
+    return false
 } 
 
 
 function getUsersNamesInAgeRange(people, gender) {
-    let result = 0
-    if (people.length === 0) {result = 0}
-    else if (gender === "мужской") {
-        result = (people.filter(user => user.gender === "мужской").map(user => user.age).reduce((acc, age) => acc + age, 0)) / people.filter(user => user.gender === "мужской").length
-    }   
-    else if (gender === "женский") {
-        result = (people.filter(user => user.gender === "женский").map(user => user.age).reduce((acc, age) => acc + age, 0)) / people.filter(user => user.gender === "женский").length
-    }
-    return result
+    return people
+        .filter(item => item.gender === gender)
+        .reduce((acc, item, index, arr) => {
+            return acc + item.age / arr.length
+        }, 0)
 }
