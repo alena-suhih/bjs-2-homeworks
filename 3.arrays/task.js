89d29740966b68f331535990ef9001f59a99ad33
alena-suhih <prhalenka23@gmail.com>
2023-01-09 15:28:44 +0200

    Задание 1 и 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length !== arr2.length) {
+        return false
+    } else if (arr1.every ((element, index) => element === arr2[index])) {
+        return true
+    } else return false
+} 
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+
+function getUsersNamesInAgeRange(people, gender) {
+    let result = 0
+    if (people.length === 0) {result = 0}
+    else if (gender === "мужской") {
+        result = (people.filter(user => user.gender === "мужской").map(user => user.age).reduce((acc, age) => acc + age, 0)) / people.filter(user => user.gender === "мужской").length
+    }   
+    else if (gender === "женский") {
+        result = (people.filter(user => user.gender === "женский").map(user => user.age).reduce((acc, age) => acc + age, 0)) / people.filter(user => user.gender === "женский").length
+    }
+    return result
 }
